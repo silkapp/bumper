b903fddf2fbb542a741fd51f7f947e3d92c1e0fd
Bram Schuur <bram@typlab.com>
2011-11-25 09:19:11 +0100

    Updated transitive bumper to correctly discard unchanged dependencies.

--- src/Bumper.hs
+++ src/Bumper.hs
@@ -48,8 +48,8 @@ trans ps = fix (transStep ps)
 
 transStep :: Packages -> Changes -> Changes
 transStep ps old = new <.> old
-  where deps = concatMap (get dependants) $ lookupPackages (M.keys old) ps
-        new  = M.fromList $ map (\p -> (get name p, bumpPosition 2 (get version p))) $ lookupPackages deps ps
+  where deps = filter (not . null . dependencyUpdates old) ps
+        new  = M.fromList . map (\p -> (get name p, bumpPosition 2 (get version p))) $ deps
 
 fix :: (Eq a) => (a -> a) -> a -> a
 fix f a | b == a    = a
@@ -61,5 +61,7 @@ dependencyUpdates :: Changes -> Package -> [Dependency]
 dependencyUpdates ch = foldr addDep [] . get dependencies
   where addDep (Dependency n r) dps =
           case M.lookup n ch of
-            Just v  -> Dependency n (addVersionToRange v r) : dps
+            Just v  -> if withinRange v r
+                        then dps
+                        else Dependency n (addVersionToRange v r) : dps
             Nothing -> dps
